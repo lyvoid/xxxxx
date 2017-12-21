9dbe8c5872b180f17f9e2cb3273ab61e02381b86
yang <735600970@qq.com>
2017-12-21 10:03:47 +0800

    新建项目

---
+++ Assets/scripts/design_pattern/StateModeTest.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StateModeTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Context context = new Context ();
+		context.SetState (new StateA(context));
+		Debug.Log(context.Request());
+		context.SetState (new StateB (context));
+		Debug.Log(context.Request());
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
+
+
+public abstract class IState {
+	protected Context mContext = null;
+
+	public IState(Context theContext){
+		mContext = theContext;
+	}
+
+	public abstract string Handle ();
+}
+
+public class StateA : IState{
+	public StateA(Context theContext) : base(theContext){
+	}
+
+	public override string Handle(){
+		return "StateA";
+	}
+}
+
+public class StateB : IState{
+	public StateB(Context theContext) : base(theContext){
+	}
+
+	public override string Handle(){
+		return "StateB";
+	}
+}
+
+
+
+public class Context
+{
+	IState mState = null;
+
+	public string Request(){
+		return mState.Handle ();
+	}
+
+	public void SetState(IState state){
+		this.mState = state;
+	}
+}
+
